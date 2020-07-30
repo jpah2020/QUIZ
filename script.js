ca3268c741cd60db5c0f8f4041499711685e614e
Rusky <jpah.design@gmail.com>
2020-07-29 23:55:57 -0400

    change from index1 to index

---
+++ script.js
@@ -0,0 +1,119 @@
+function Quiz(questions) {
+	this.score = 0;
+	this.questions = questions;
+	this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function () {
+	return this.questions[this.questionIndex];
+};
+
+Quiz.prototype.guess = function (answer) {
+	if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+		this.score++;
+	}
+
+	this.questionIndex++;
+};
+
+Quiz.prototype.isEnded = function () {
+	return this.questionIndex === this.questions.length;
+};
+
+function Question(text, choices, answer) {
+	this.text = text;
+	this.choices = choices;
+	this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+	return this.answer === choice;
+};
+
+function populate() {
+	if (quiz.isEnded()) {
+		showScores();
+	} else {
+		// show question
+		var element = document.getElementById('question');
+		element.innerHTML = quiz.getQuestionIndex().text;
+
+		// show options
+		var choices = quiz.getQuestionIndex().choices;
+		for (var i = 0; i < choices.length; i++) {
+			var element = document.getElementById('choice' + i);
+			element.innerHTML = choices[i];
+			guess('btn' + i, choices[i]);
+		}
+
+		showProgress();
+	}
+}
+
+function guess(id, guess) {
+	var button = document.getElementById(id);
+	button.onclick = function () {
+		quiz.guess(guess);
+		populate();
+	};
+}
+
+function showProgress() {
+	var currentQuestionNumber = quiz.questionIndex + 1;
+	var element = document.getElementById('progress');
+	element.innerHTML =
+		'Question ' + currentQuestionNumber + ' of ' + quiz.questions.length;
+}
+
+function showScores() {
+	var gameOverHTML = '<h1>Result</h1>';
+	gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + '</h2>';
+	var element = document.getElementById('quiz');
+	element.innerHTML = gameOverHTML;
+}
+
+// create questions here
+var questions = [
+	new Question(
+		'What is the name of the Vice Presidente of United States now?',
+		['Mike pence', 'Obama', 'Trump', 'pelosy'],
+		'Mike pence'
+	),
+	new Question(
+		'Name one war fought by the United States in the 1800s.',
+		['Civil War', 'Covid19', 'Cold War', 'Korean War'],
+		'Civil War'
+	),
+	new Question(
+		'What did the Declaration of Independence do?',
+		[
+			'announced war)',
+			'announced that we lost states',
+			'announced a new president',
+			'announced our independence (from Great Britain)',
+		],
+		'announced our independence (from Great Britain)'
+	),
+	new Question(
+		'What is an amendment?',
+		['a book', 'a change (to the Constitution)', 'a movie', 'All'],
+		'a change (to the Constitution)'
+	),
+	new Question(
+		'What is the name of the national anthem?',
+		[
+			'stairway to heaven',
+			'dream on',
+			'The Star-Spangled Banner',
+			'Run to the hills',
+		],
+		'The Star-Spangled Banner'
+	),
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
+
